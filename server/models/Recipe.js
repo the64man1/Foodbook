a629e7ebba29035b76a15597ed76f6d8999a4410
maxquintanilla <the64man1@yahoo.com>
2021-07-31 13:29:44 -0500

    models starters

---
+++ server/models/Recipe.js
@@ -0,0 +1,19 @@
+// TODO: This whole model
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const recipeSchema = new Schema({
+    createdBy: [ User ],
+    ingredients: [],
+    instructions: '',
+    numberOfLikes: Number,
+    numberOfDislikes: Number,
+    comments: [],
+    image: '',
+    public: Boolean
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
