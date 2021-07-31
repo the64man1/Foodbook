d5e6dd509cf25004c91e4af0c9ab4e8418563c7f
maxquintanilla <the64man1@yahoo.com>
2021-07-31 13:52:19 -0500

    category model/seed

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
 const User = require('./User');
+const Recipe = require('./Recipe');
+const Category = require('./Category');
 
-module.exports = { User };
+module.exports = { User, Recipe, Category };
