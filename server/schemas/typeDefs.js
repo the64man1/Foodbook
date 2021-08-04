44de74a768f17e7b587d615512fb6cff05dc5acb
maxquintanilla <the64man1@yahoo.com>
2021-08-03 20:19:34 -0500

    develop resolvers and typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,62 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type User {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+        savedRecipes: [Recipe]
+        createdRecipes: [Recipe]
+    }
+
+    type Recipe {
+        _id: ID
+        createdBy: [User]
+        ingredients: [String]
+        instructions: String
+        numberOfLikes: Int
+        numberOfDislikes: Int
+        comments: [String]
+        image: String
+        public: Boolean
+        categores: [Category]
+    }
+    
+    type Category {
+        _id: ID
+        name: String
+    }
+
+    type Query {
+        me: User
+        allRecipes: [Recipe]
+        singleRecipe: Recipe
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input NewRecipeInput {
+        _id: ID
+        createdBy: [User]
+        ingredients: [String]
+        instructions: String
+        numberOfLikes: Int
+        numberOfDislikes: Int
+        comments: [String]
+        image: String
+        public: Boolean
+        categores: [Category]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addRecipe(recipe: NewRecipeInput) : Recipe
+    }
+`;
 
 module.exports = typeDefs;
