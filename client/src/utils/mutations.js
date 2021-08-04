7f44e1f7790667ca4fbfd1549fff32ebdf48315a
Kthendavid04 <kthendavid04@gmail.com>
2021-08-03 19:49:26 -0500

    created exports for mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,4 +2,20 @@ import { gql } from '@apollo/client';
 
 export const LOGIN = gql``;
 
+
 export const ADD_USER = gql``;
+
+export const CREATE_RECIPE = gql``;
+
+export const DELETE_RECIPE = gql``;
+
+export const UPDATE_RECIPE = gql``;
+
+
+
+//mutations: 
+// add user
+// login
+// create recipe
+// delete recipe (creator can delete)
+// update recipe (creator can update)
