bf36e23bd70e1c49a6abda2b20912c7d8be9ee3b
maxquintanilla <the64man1@yahoo.com>
2021-08-05 18:49:41 -0500

    backend changes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -55,7 +55,7 @@ const typeDefs = gql`
 
     type Mutation {
         login(email: String!, password: String!): Auth
-        addUser(firstName: String!, lastName: String!, email: String!, password: String!): User
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
         addRecipe(recipe: NewRecipeInput): Recipe
     }
 `;
