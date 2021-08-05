242c96cf9c60b3e084bcd0520379852eda9222b3
Antonio Quintanilla <the64man1@yahoo.com>
2021-08-05 18:27:14 -0500

    Create Recipe.js

---
+++ server/models/Recipe.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const recipeSchema = new Schema({
+  createdBy: { type: Schema.Types.ObjectId, ref: "User" },
+  title: { type: String },
+  ingredients: [{ type: String }],
+  instructions: { type: String },
+  numberOfLikes: { type: Number },
+  numberOfDislikes: { type: Number },
+  comments: [{ type: String }],
+  image: { type: String },
+  public: { type: Boolean },
+  categories: [{ type: Schema.Types.ObjectId, ref: "Category" }],
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
