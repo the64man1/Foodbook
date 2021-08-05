f50d7a45e435411e55b4093c5fdb073cab10f333
christian louis <clouqb12@yahoo.com>
2021-08-05 18:25:23 -0500

    created the home component and got it to load, but no css.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@ap
 import { setContext } from "@apollo/client/link/context";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
 	uri: "/graphql",
@@ -37,8 +38,11 @@ function App() {
             <Route exact path="/">
               <Login />
             </Route>
-            <Route  path="/signup">
+            <Route path="/signup">
               <Signup />
+            </Route>
+            <Route path="/home">
+              <Home />
             </Route>
           </Switch>
         </>
