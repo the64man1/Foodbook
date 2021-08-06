46834d2876c632cb0142fbaf01c7cc4a224ab3ac
Kthendavid04 <kthendavid04@gmail.com>
2021-08-05 21:11:39 -0500

    created QUERY_USER and QUERY_RECIPE

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,29 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql``;
+export const QUERY_USER = gql`
+    query user {
+        _id
+        username
+        firstname
+        lastName
+        email
+        password
+        savedRecipes
+        createdRecipes
+        
+    }`;
+
+
+
+export const QUERY_RECIPE = gql`
+    query recipe {
+        _id
+        title
+        categories
+        ingredients
+        instructions
+        likes
+        dislikes
+        comments
+        public
+    }`;
