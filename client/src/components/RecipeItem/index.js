d7740af18f202929fdcd3528b1c8a38627e5b07e
syed Rizvi <rizvi5a@yahoo.com>
2021-08-08 16:18:08 -0500

    fixed some typos in feed.js

--- client/src/components/RecipeItem/index.js
+++ client/src/components/RecipeItem/index.js
@@ -18,7 +18,7 @@ function RecipeItem(recipe) {
     return (
       <div className="card">
         <div className="card-body">
-          <img width="200" height="200" src={`/images/${image}` }/>
+          <img width="200" height="200" alt= {image} src={`/images/${image}` }/>
           <h5 className="card-title">{title}</h5>
           <p class="card-text">{ingredients}</p>
           <p class="card-text">{instructions}</p>
