f29dc4795de48bf8b8e4bcac71138e1c6265c7a2
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-08-07 19:57:29 -0500

    Basic working sign up front to back end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,126 +1,125 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
-mutation login($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-    token
-    user {
+      token
+      user {
         _id
         username
-    }
-    }
-}`;
-
-
-export const ADD_USER = gql`
-    mutation {
-        addUser(input: {...}) {
-            firstName
-            lastName
-            email
-            password
-        }
-
-        input firstName{
-            type: String!
-        }
-
-        input lastName{
-            type: String!
-        }
-
-        input eamil{
-            type: String!
-        }
-
-        input password{
-            type:String!
-        }
-    }`;
-
-export const CREATE_RECIPE = gql`
-    mutation {
-        createRecipe(input:{...}) {
-            createdBy
-            title
-            ingredients
-            instructions
-            likes {
-                username
-                likedOn
-            }
-            dislikes {
-                username
-                dislikedOn
-            }
-            comments {
-                username
-                comment
-                commentedOn
-            }
-            image
-            public
-            categories
-        }
-
-        input createdBy{
-            type: Schema.Types.ObjectId, ref: "User"
-        }
-
-        input title{
-            type: String!
-        }
-
-        input ingredients{
-            type: String!
-        }
-
-        input instructions{
-            type: String!
-        }
-
-        input numberOfLikes{
-            type: Number!
-        }
-
-        input numberOfDislikes{
-            type: Number!
-        }
-
-        input comments{
-            type: [],
-        }
-
-        input image{
-            type:"",
-        }
-
-        input public{
-            type: Boolean!
-        }
-
-        input categories{
-            type:[Category.schema]
-        }
+      }
     }
   }
 `;
 
+export const ADD_USER = gql`
+  mutation addNewUser(
+    $username: String!
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $username
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      user {
+        id
+        username
+        firstName
+        lastName
+        email
+      }
+      token
+    }
+  }
+`;
+
+// export const CREATE_RECIPE = gql`
+//     mutation {
+//         createRecipe(input:{...}) {
+//             createdBy
+//             title
+//             ingredients
+//             instructions
+//             likes {
+//                 username
+//                 likedOn
+//             }
+//             dislikes {
+//                 username
+//                 dislikedOn
+//             }
+//             comments {
+//                 username
+//                 comment
+//                 commentedOn
+//             }
+//             image
+//             public
+//             categories
+//         }
+
+//         input createdBy{
+//             type: Schema.Types.ObjectId, ref: "User"
+//         }
+
+//         input title{
+//             type: String!
+//         }
+
+//         input ingredients{
+//             type: String!
+//         }
+
+//         input instructions{
+//             type: String!
+//         }
+
+//         input numberOfLikes{
+//             type: Number!
+//         }
+
+//         input numberOfDislikes{
+//             type: Number!
+//         }
+
+//         input comments{
+//             type: [],
+//         }
+
+//         input image{
+//             type:"",
+//         }
+
+//         input public{
+//             type: Boolean!
+//         }
+
+//         input categories{
+//             type:[Category.schema]
+//         }
+//     }
+//   }
+// `;
+
 export const DELETE_RECIPE = gql`
-    mutation($id: ID) {
-        deleteRecipe(id: $id)
-    }`;
+  mutation ($id: ID) {
+    deleteRecipe(id: $id)
+  }
+`;
 
-export const UPDATE_RECIPE = gql``;
+// export const UPDATE_RECIPE = gql``;
 
-
-
-//mutations: 
+//mutations:
 // add user
 // login
 // create recipe
 // delete recipe (creator can delete)
 // update recipe (creator can update)
-
 
 // mutation ADD_USER ($input: UpdatePostInput! { updatePost(input: $input) {...}})
