0e972148cdfea93f3338db3da9d3f7b46c220a71
Kthendavid04 <kthendavid04@gmail.com>
2021-08-12 19:07:13 -0500

    udated front end

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -4,16 +4,26 @@ import { Container } from 'semantic-ui-react'
 
 const Welcome = () => {
     return (
-      <Container>
+      <Container style={{
+        background:' rgba( 255, 255, 255, 0.25 )',
+        boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 ',
+        backdropFilter: 'blur( 4px )',
+        WebkitBackdropFilter: 'blur( 4px )',
+        borderRadius: '10px',
+        border:' 1px solid rgba( 255, 255, 255, 0.18 )'
+      }}>
         {Auth.loggedIn() ? (
           <p style={{ textAlign: "center", width: "100%", fontSize: "1.5rem", lineHeigh: "3rem" }}>
             Checkout the Foodbook for recipes!
           </p>
         ) : (
-          <p style={{ textAlign: "center", width: "100%", fontSize: "1.5rem",  lineHeigh: "3rem" }}>
-            Welcome to the Foodbook! We have awesome recipes to try, generated
-            by our users. Create an account to add your favorite recipes to the
-            Foodbook!
+          <p style={{ textAlign: "center", width: "100%", fontSize: '32px',  lineHeigh: "18rem" 
+          }}>
+            foodBook...
+            <br></br>
+       
+            Your digital cookbook. 
+           Login and search, save, share and even add your recipes. 
           </p>
         )}
       </Container>
